e44eee9037b3edf535d0ca62cf6ff9295327356a
bashirahmed-cse <bashirahmed.cse@gmail.com>
2023-01-15 19:01:21 +0600

    update image

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -4,6 +4,14 @@ include "config.php";
 $post_id=$_GET['id'];
 $cat_id=$_GET['catid'];
 
+/*   folder theke image delete     */
+$sql1 ="SELECT * FROM post WHERE post_id ={$post_id}";
+$result = mysqli_query($conn,$sql1);
+$row=mysqli_fetch_assoc($result);
+
+unlink("upload/".$row['post_img']);
+
+
 $sql="DELETE FROM post WHERE post_id = {$post_id};";
 $sql .="UPDATE category SET post = post - 1 WHERE category_id={$cat_id}";
 
